3499ceaa5f298c188347224dc573bcf6ab057b5d
Mike McGowan <mike.mcgowan.75@gmail.com>
2024-08-16 19:39:36 +0100

    Minor change

--- src/main/kotlin/com/github/mikemcgowan/runnerz/Application.kt
+++ src/main/kotlin/com/github/mikemcgowan/runnerz/Application.kt
@@ -1,25 +1,23 @@
 package com.github.mikemcgowan.runnerz
 
+import com.github.mikemcgowan.runnerz.run.Location
+import com.github.mikemcgowan.runnerz.run.Run
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import com.github.mikemcgowan.runnerz.Application
-import com.github.mikemcgowan.runnerz.run.Run
-import com.github.mikemcgowan.runnerz.run.Location
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 
 @SpringBootApplication
 class Application {
     @Bean
-    fun commandLineRunner(): CommandLineRunner =
-        CommandLineRunner { _ ->
-            val now = LocalDateTime.now()
-            val run = Run(1, "First run", now, now.plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR)
-            logger.info(run.toString())
-        }
+    fun commandLineRunner(): CommandLineRunner = CommandLineRunner {
+        val now = LocalDateTime.now()
+        val run = Run(1, "First run", now, now.plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR)
+        logger.info(run.toString())
+    }
 
     companion object {
         private val logger = LoggerFactory.getLogger(Application::class.java)
